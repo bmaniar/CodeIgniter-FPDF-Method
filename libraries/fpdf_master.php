08894ee83a0deba866ca4279fee1943bc8b42bbe
bmaniar00 <bmaniar00@gmail.com>
2016-02-24 13:17:28 +0530

    Library of FPDF

---
+++ libraries/fpdf_master.php
@@ -0,0 +1,17 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Fpdf_gen {
+		
+	public function __construct() {
+		
+		require_once APPPATH.'third_party/fpdf/fpdf-1.7.php';
+		
+		$pdf = new FPDF();
+		$pdf->AddPage();
+		
+		$CI =& get_instance();
+		$CI->fpdf = $pdf;
+		
+	}
+	
+}
