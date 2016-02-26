5c023cd38d7a1cd2e2aa446e2602139cc05ab43a
bmaniar00 <bmaniar00@gmail.com>
2016-02-26 22:06:33 +0530

    File name capitalization

--- controllers/fpdf_file.php
+++ controllers/fpdf_file.php
@@ -10,11 +10,15 @@ class Fpdf extends CI_Controller {
 	 *
 	 */
 	public function index() {	
-		$this->load->library('fpdf_master);
+		$this->load->library('fpdf_master');
 		
 		$this->fpdf->SetFont('Arial','B',18);
+		
 		$this->fpdf->Cell(50,10,'Hello World!');
 		//All text which have to print should be goes here
-		echo $this->fpdf->Output('hello_world.pdf','D');// Name of PDF file 
+		//also you can go for calling view over here and put the same type of code inside the view
+		
+		echo $this->fpdf->Output('hello_world.pdf','D');// Name of PDF file
+		//Can change the type from D=Download the file		
 	}
 }
--- libraries/fpdf_master.php
+++ libraries/fpdf_master.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Fpdf_gen {
+class Fpdf_master {
 		
 	public function __construct() {
 		
